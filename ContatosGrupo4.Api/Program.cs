9435680c61e5b9840bb9201fd5cc2d916de3fc89
arthuurw <arthurwm_kta@hotmail.com>
2025-07-06 17:39:47 -0300

    Atualização ks8
    Atualização de arquivos yml para utilização do Kubernetes.

--- ContatosGrupo4.Api/Program.cs
+++ ContatosGrupo4.Api/Program.cs
@@ -24,11 +24,8 @@ builder.Services.AddHostedService<ContatoConsumerService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
